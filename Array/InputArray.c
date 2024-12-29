d3d981d24d504a640a52222171e7773dd79eb6d9
Aayush Raut <aayushraut209@gmail.com>
2024-12-29 21:22:23 +0545

    Update InputArray.c

--- Array/InputArray.c
+++ Array/InputArray.c
@@ -1,1 +1,23 @@
+/*
+    This C program to read elements of array from user
+*/
 
+#include<stdio.h>
+
+int main(){
+
+    int arr[4]; //array of integer with size 4
+    int i;
+
+    //Reading elements of array
+    for(i=0; i<4; i++){
+        printf("Enter arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+
+    }
+    
+
+
+    printf("\n");
+    return 0;
+}
