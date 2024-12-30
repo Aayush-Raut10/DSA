e6ba2c7a3bc54262b6981ea3c376a8614ea03bf9
Aayush Raut <aayushraut209@gmail.com>
2024-12-30 12:26:38 +0545

    Add files via upload

---
+++ Array/sumofArray.c
@@ -0,0 +1,36 @@
+/*
+    This program demonstrates the sum of elements of an array
+    (Lets suppose we need to sum up marks of 5 subjects of a student)
+    
+*/
+#include<stdio.h>
+#define SIZE 5
+
+
+int main(){
+
+    int i,sum_of_marks = 0;
+
+    int marks[SIZE]; //Array for storing marks of 5 subject
+
+
+    //Taking marks of each 5 subjects
+    for(i=0; i<SIZE; i++){
+        printf("Enter the marks of subject %d = ",i+1);
+        scanf("%d",&marks[i]);
+    }
+
+    
+    //Adding elements of array 
+    for(i=0; i<SIZE; i++){
+            sum_of_marks = sum_of_marks + marks[i];
+    }
+
+
+    printf("Sum of marks in 5 subjects = %d\n",sum_of_marks);
+
+
+
+
+    return 0;
+}
