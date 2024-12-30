1fb2cab9cd37315939cf6b9ff8c0c18564ac5760
Aayush Raut <aayushraut209@gmail.com>
2024-12-30 12:09:48 +0545

    Add files via upload

---
+++ Array/outputArray.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main(){
+
+    int arr[] = {5,-6,18,2,10,85};
+    int i;
+
+    //Displaying elements of array
+    for(i=0; i<6; i++){
+        printf("arr[%d] = %d\n",i,arr[i]);
+    }
+
+    return 0;
+}
