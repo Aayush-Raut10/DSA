cad33ba64454ee8bdcc19892b5911f91225812e1
Aayush Raut <aayushraut209@gmail.com>
2025-01-01 10:54:33 +0545

    Update 2DArray.c
    Add Output

--- Array/2DArray.c
+++ Array/2DArray.c
@@ -9,9 +9,9 @@ int main(){
     int Matrix[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
     //Accessing elements of 2D array
-    printf("Matrix[%d][%d] = %d\n",0,2,Matrix[0][2]);
-    printf("Matrix[%d][%d] = %d\n",2,3,Matrix[2][3]);
-    printf("Matrix[%d][%d] = %d\n",1,0,Matrix[1][0]);
+    printf("Matrix[%d][%d] = %d\n",0,2,Matrix[0][2]); // Output is 3
+    printf("Matrix[%d][%d] = %d\n",2,3,Matrix[2][3]); // Output is 12
+    printf("Matrix[%d][%d] = %d\n",1,0,Matrix[1][0]); // Output is 5
     
 
     
