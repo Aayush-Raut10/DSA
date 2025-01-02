2d7c1f7040c1e9505388e183149b88caf1027c92
Aayush Raut <aayushraut209@gmail.com>
2025-01-02 10:00:53 +0545

    Update searchin2DArray.c

--- Array/searchin2DArray.c
+++ Array/searchin2DArray.c
@@ -5,11 +5,11 @@ int main(){
 
     int i,j;
 
-    int Matrix[3][3] = {{-9,56,3},{7,85,-2},{4,5,8}};
+    int Matrix[3][3] = {{-9,56,3},{7,85,-2},{4,5,8}}; // Initializing a 2D Array
 
     int target = 85,index1,index2;
 
-    //Search for target elements in an 2D array
+    //Search for targeted elements in an 2D array
     for(i=0; i<3; i++){
         for(j=0; j<3; j++){
 
@@ -22,7 +22,7 @@ int main(){
         }
         }
     
-
+    
     printf("Position of targeted element in 2D array = (%d,%d)\n",index1,index2);
    
 
