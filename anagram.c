52c19387e56d941a6a11b98bcc30488bdc310fdc
Aayush Raut <aayushraut209@gmail.com>
2025-05-19 13:33:23 +0545

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -2,29 +2,29 @@
     C program to check anagram 
 */
 #include <stdio.h>
-  void sort(int arr[]){
+  void sort(char arr[]){
       int i,j;
     for(i=0; i<5; i++){
         for(j=0; j<5-i-1; j++){
             if(arr[j]>arr[j+1]){
-                int temp = arr[j];
+                char temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
     }
     }
-    void display(int arr[]){
+    void display(char arr[]){
         for(int k = 0; k<5; k++){
-            printf("%d ",arr[k]);
+            printf("%c ",arr[k]);
         }
         printf("\n");
     }
 
 int main() {
     int i,j,check=0;
-    int arr1[5] = {1, 20, 50, 6, 48};
-    int arr2[5] = {1, 50, 48, 20, 6};
+    char arr1[5] = {'H', 'E', 'A', 'R', 'T'};
+    char arr2[5] = {'E', 'A', 'R', 'T', 'H'};
      sort(arr1);
      sort(arr2);
      display(arr1);
