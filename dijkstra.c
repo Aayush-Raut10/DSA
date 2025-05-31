c42a460c61b88e01c7be0c8402637e3eab261bb0
Aayush Raut <aayushraut209@gmail.com>
2025-05-31 16:45:15 +0545

    Add files via upload

---
+++ dijkstra.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define V 5
+
+void dijkstra(int graph[V][V], int src) {
+    int dist[V];
+    int visited[V] = {0};
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+    }
+    dist[src] = 0;
+    for (int count = 0; count < V - 1; count++) {
+        int u, min_dist = INT_MAX;
+        for (int v = 0; v < V; v++) {
+            if (!visited[v] && dist[v] < min_dist) {
+                min_dist = dist[v];
+                u = v;
+            }
+        }
+        visited[u] = 1;
+        for (int v = 0; v < V; v++) {
+            if (!visited[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+    printf("Vertex\t\tDistance from Source\n");
+    for (int i = 0; i < V; i++) {
+        printf("%d \t\t\t %d\n", i, dist[i]);
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+    int source;
+    
+    printf("Enter source vertex: ");
+    scanf("%d",&source);
+    
+    printf("Source = %d\n",source);
+    dijkstra(graph, source);
+    return 0;
+}
